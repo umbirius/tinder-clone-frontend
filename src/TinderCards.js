17b45c03bf6282162a8a50c8a828c62787785f51
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-11 00:26:32 -0500

    TinderCard now displays people + styling

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TinderCards.css'
+import TinderCard from 'react-tinder-card'
+import { SwipeableDrawer } from '@material-ui/core'
 
 function TinderCards() {
-    return <div className="tinderCards"> </div>
+    const [people, setPeople] = useState([
+        {
+            name: 'Chuckee Cheese',
+            url: "https://media2.fdncms.com/metrotimes/imager/u/blog/24545547/screen_shot_2020-05-18_at_11.17.38_am.png?cb=1589832349"
+        },
+        {
+            name: 'Dinosaur',
+            url: "https://i.insider.com/5f08b7e5f34d053f663fbada"
+        },
+    ])
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        // setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
+
+
+    return <div className="tinderCards">
+        <div className="tinderCards__cardContainer">
+            {people.map(person => (
+                <TinderCard
+                    className="swipe"
+                    key={person.name}
+                    prevenetSwipe={["up", "down"]}
+                    onSwipe={(dir) => swiped(dir, person.name)}
+                    onCardLeftScreen={() => outOfFrame(person.name)}
+                >
+                    <div
+                        style={{ backgroundImage: `url(${person.url})`}} 
+                        className="card">
+                        <h3>{person.name}< /h3>
+                    </div>
+                </TinderCard>
+            ))}
+
+        </div>
+    </div>
 }
 
 export default TinderCards
