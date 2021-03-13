3f7c623a57838aa5a9a6f5e3a9539a31525678cb
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-13 10:59:36 -0500

    made swipe button class + css

---
+++ src/SwipeButtons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SwipeButtons.css"
+
+function SwipeButtons(){
+    return <div className="swipeButtons">
+        
+    </div>
+}
+
+export default SwipeButtons
